01378a0d530490d2be4587b898f06a7f68706bf3
Marta <dilararandt@gmail.com>
2025-02-11 11:03:26 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a second project from the course "100 Days of Code: The Complete Python Pro Bootcamp".
+The task is to build your own portfolio website.
+
+I used Flask and built this portfolio page in html.
+I didn't include the images of my certificates as they have my personal data on them.
+Please see the screenshots below of how the final page looks like.
+
+Feel free to advise how to improve the code.
+
+Thanks!
---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/developer")
+def dev():
+    return render_template('DEV.html')
+
+
+@app.route("/economy_analyst")
+def econ():
+    return render_template('ECON.html')
+
+
+@app.route("/hobby")
+def hobby():
+    return render_template('HOBBY.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
